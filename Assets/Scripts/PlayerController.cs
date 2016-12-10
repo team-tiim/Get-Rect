731ae943edefc0360dd4c423b546b7e0a5d6ff7d
tyyp <sander.sats@gmail.com>
2016-12-10 15:38:07 +0200

    Added spawn dude button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,33 +6,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    Rigidbody2D rb;
+    public float speed;             //Floating point variable to store the player's movement speed.
+    public float jumpPower;
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
     // Use this for initialization
     void Start()
     {
-        rb = this.gameObject.GetComponent<Rigidbody2D>();
-        Debug.Log(rb.transform.position.x);
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        Debug.Log(rb2d.transform.position.x);
     }
 
-    // Update is called once per frame
-    void Update()
+    bool IsGrounded()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        return rb2d.velocity.y == 0;
+    }
+
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
         {
-            Debug.Log("Pressed A");
-            rb.AddForce(Vector2.left * 1, ForceMode2D.Impulse);
-            rb.AddForce(Vector3.forward * 1, ForceMode2D.Impulse);
+            rb2d.AddForce(new Vector2(0, rb2d.mass * jumpPower), ForceMode2D.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            rb.AddForce(Vector3.right * 1, ForceMode2D.Impulse);
-            rb.AddForce(Vector3.forward * 1, ForceMode2D.Impulse);
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Vector3 dir = new Vector3(-10f, 15f, 0f);
-            dir.Normalize();
-            rb.AddForce(Vector3.up * 1, ForceMode2D.Impulse);
-        }
+
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, rb2d.velocity.y);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        //rb2d.AddForce(movement * speed);
+        rb2d.velocity = movement;
+
+
+
     }
 }
