15227191ba692b5ce1dc68ece3fc133648bc7120
Tyhisilm <tyhisilm@gmail.com>
2017-09-22 20:34:59 +0300

    Revert "Merge branch 'master' of https://github.com/team-tiim/Get-Rect"
    This reverts commit c6ba9cc4c1ee972e7a98b8810bac8a4910ac297c, reversing
    changes made to 29f64201bb6810b78c134c224bb75394778d7115.

---
+++ Assets/Scripts/DudeSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeSpawner : MonoBehaviour {
+
+    public GameObject[] prefabs;
+    public int spawnTick = 15; //seconds
+    public bool isEnabled = true;
+    private int counter = 0;
+
+    void Start()
+    {
+        InvokeRepeating("MakeDude", 2, spawnTick);
+    }
+
+    public void MakeDude()
+    {
+        if (isEnabled)
+        {
+            int arrayIdx = Random.Range(0, prefabs.Length);
+            GameObject newDude = Instantiate(prefabs[arrayIdx]);
+            newDude.name = name;
+            newDude.transform.position = this.transform.position;
+
+            Powerup powerup = newDude.GetComponent<Powerup>();
+            if (powerup != null)
+            {
+                powerup.spawner = this;
+            }
+        }
+    }
+}
