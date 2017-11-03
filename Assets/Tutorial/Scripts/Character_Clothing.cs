973965449ada496e6083a84343d495dcb538bd9a
Silver <silverkontus@gmail.com>
2017-11-03 13:20:14 +0200

    Animations
    Something is broken...

---
+++ Assets/Tutorial/Scripts/Character_Clothing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Anima2D;
+
+public class Character_Clothing : MonoBehaviour {
+
+    public SpriteMeshAnimation spr_anim;
+
+	public void ChangeHat()
+    {
+        int frames = spr_anim.frames.Length;
+        int currFrame = spr_anim.frame;
+
+        if (currFrame + 1 < frames)
+        {
+            currFrame += 1;
+        }
+        else
+        {
+            currFrame = 0;
+        }
+
+        spr_anim.frame = currFrame;
+        
+    }
+}
