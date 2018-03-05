f1a0803aad39989fb4964fb9d8847199f1695e05
pihels <pihels@gmail.com>
2018-03-05 08:56:04 +0200

    flamethrower fixes

---
+++ Assets/Scripts/Effects/OngoingDamageEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OngoingDamageEffect : MonoBehaviour {
+    private static readonly float ONE_SECOND = 1.0f;
+
+    public float duration = 3.0f;
+    public int dps = 1;
+
+    private float nextActionTime = 0.0f;
+
+    void Start () {
+
+    }
+
+    void Update()
+    {
+        if (Time.time > nextActionTime) {
+            nextActionTime = Time.time + ONE_SECOND;
+            gameObject.GetComponent<CharacterBehaviourBase>().TakeDamage(dps);
+        }
+    }
+
+
+}
